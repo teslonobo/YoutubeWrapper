1198f0e56988d19987a3abf3d1d51e567c5f8c57
Tes <139300046+teslonobo@users.noreply.github.com>
2024-07-03 12:11:20 -0400

    Delete Test
    outdated test file

--- Test.py
+++
@@ -1,8 +0,0 @@
-from YouTubeDL_Wrapper import YouTube_DL
-
-
-url = 'https://www.youtube.com/watch?v=viAk4pUCzSs'
-url = 'https://www.youtube.com/watch?v=T6eK-2OQtew&list=RDCLAK5uy_k1VVBVsS6pu1pVkYZK2B0EWic3i4j_TY4&index=2'
-option = 'Audio'
-
-youtube_link = YouTube_DL(url,option).download_playlist()
